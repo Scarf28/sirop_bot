d5c3126bb85375e559fd7ae1045f858a7c88a486
DALLOU Florian <florian.dallou@gmail.com>
2015-12-04 17:32:17 +0100

    maj api et main_simu

--- Code_C/main.c
+++ Code_C/main.c
@@ -1,20 +1,140 @@
 #include <stdio.h>
+#include <math.h>
 #include "robot_api.h"
+
+
+
+int creer_mur(s_mur* tab_mur,float Xdebut, float Ydebut, float Xfin, float Yfin);
+void move_robot(s_robot* tab_robot, s_mur* tab_mur, int id, float speed, float angle);
+
+//variables globales
+int largeur_terrain = 800;
+int hauteur_terrain = 600;
 
 int main()
 {
 
-largeur_terrain = 800;
-hauteur_terrain = 600;
+
+int debug=0;
+
 
 //déclaration des tableaux de mur, robot et missile
 s_mur tab_mur[100] = {};
-s_robot tab_robot[10]
-s_missile tab_missile[30];
+s_robot tab_robot[10] = {};
+s_missile tab_missile[30] = {};
 
 //initialisation des murs
-tab_mur[0] = 
+creer_mur(tab_mur,0,0,0,hauteur_terrain);
+creer_mur(tab_mur,0,hauteur_terrain,largeur_terrain,hauteur_terrain);
+creer_mur(tab_mur,largeur_terrain,hauteur_terrain,largeur_terrain,0);
+creer_mur(tab_mur,largeur_terrain,0,0,0);
+
+//initialisation des robots
+tab_robot[0].x = 40;
+tab_robot[0].y = 40;
+tab_robot[0].teta = 0.40;
+tab_robot[0].existe = 1;
+tab_robot[1].x = 500;
+tab_robot[1].y = 450;
+tab_robot[1].teta = 0.8;
+tab_robot[1].existe = 1;
+
+move_robot(tab_robot,tab_mur,1,7,M_PI/2);
+int i =0;
+long int delay = 0;
+/*
+for (i=0; i<10;i++)
+{
+delay =0;
+
+
+while(tab_robot[0].y < 100)
+{
+
+
+while (delay < 1000000000)
+{
+delay++;
+}
+move_robot(tab_robot,tab_mur,0,10,0.2);
+}
+printf("robot pos :x= %f, y=%f, teta = %f\n",tab_robot[0].x,tab_robot[0].y,tab_robot[0].teta) ;
+
+while(tab_robot[0].y >10)
+{
+move_robot(tab_robot,tab_mur,0,-10,0.2);
+}
+printf("robot pos :x= %f, y=%f, teta = %f\n",include <math.h> tab_robot[0].x,tab_robot[0].y,tab_robot[0].teta) ;
+}
+*/
+//printf("robot pos :x= %f, y=%f, teta = %f\n",tab_robot[0].x,tab_robot[0].y,tab_robot[0].teta) ;
+//printf("robot pos :x= %f, y=%f, teta = %f\n",tab_robot[1].x,tab_robot[1].y,tab_robot[1].teta) ;
+for (i=0; i<80;i++)
+{
+move_robot(tab_robot,tab_mur,0,10,M_PI/4);
+printf("robot pos :x= %f, y=%f, teta = %f\n",tab_robot[0].x,tab_robot[0].y,tab_robot[0].teta) ;
+move_robot(tab_robot,tab_mur,0,10,M_PI/4);
+printf("robot pos :x= %f, y=%f, teta = %f\n",tab_robot[0].x,tab_robot[0].y,tab_robot[0].teta) ;
+}
 
 
 return 1;
 }
+
+void move_robot(s_robot* tab_robot, s_mur* tab_mur, int id, float speed, float angle)
+{
+	float x_temp = tab_robot[id].x;
+	float y_temp = tab_robot[id].y;
+	x_temp += speed * cos(angle);
+	y_temp += speed * sin(angle);
+	
+	//test des limites du terrain en x
+	if (x_temp >= largeur_terrain)
+		tab_robot[id].x = largeur_terrain;
+	else if (x_temp <= 0)
+				tab_robot[id].x = 0;
+	else	
+		tab_robot[id].x = x_temp;
+
+	//test des limites du terrain en y	
+	if (y_temp >= hauteur_terrain)
+		tab_robot[id].y = hauteur_terrain;
+	else if (y_temp <= 0)
+		tab_robot[id].y = 0;
+	else
+		tab_robot[id].y = y_temp;
+
+	tab_robot[id].teta = angle;
+}
+
+/* Création Mur */
+int creer_mur(s_mur* tab_mur,float Xdebut, float Ydebut, float Xfin, float Yfin)
+{
+	//verification que la position des murs soient compris dans le terrain sinon retourne -1
+	if ((Xdebut < 0 || Xdebut > largeur_terrain) || (Ydebut < 0 || Ydebut > hauteur_terrain) || (Xfin < 0 || Xfin > largeur_terrain) || (Yfin < 0 || Yfin > hauteur_terrain ))
+		return -1;
+	else
+	{
+		int i = 0;
+		//permet de completer la tab mur si d'autres murs existent
+		while (tab_mur[i].existe !=0 && i <101)
+		{
+			i++;
+		}
+		//si plus de 100 murs, ajout impossible, retourne -2
+		if (i > 100)
+			return -2;
+
+		tab_mur[i].Xdebut = Xdebut;
+		tab_mur[i].Ydebut = Ydebut;
+		tab_mur[i].Xfin = Xfin;
+		tab_mur[i].Yfin = Yfin;
+		tab_mur[i].existe = 1;
+	}
+return 1 ;
+}
+
+
+
+/* Affichage Mur */
+
--- Code_C/robot_api.h
+++ Code_C/robot_api.h
@@ -1,4 +1,4 @@
-// robot_api.h
+// robot_api.h version 0.1 du 04/12/15
 /* liste des fonctions pour le simulateur*/
 
 
@@ -8,36 +8,38 @@
 #include <stdio.h>
 
 //definition des structures
-struct s_robot{
+typedef struct {
 	float x;	//position x en metre du centre du robot
 	float y;	//position y en metre du cnetre du robot
 	float teta;	//angle en radian (entre 0 et 2pi)
 	int existe; //0 = pas de robot; 1 = robot présent
-};
+}s_robot;
 
-struct s_missile{
+typedef struct {
     float x;    //position x en metre en tête du missile
     float y;    //position y en metre en tête du missile
     float teta; //angle relatif du missile pas rapport à l'axe x en radian
     int existe; //0 = pas de missile; 1 = missile présent
-}
+}s_missile;
 
 //definit les points de depart et de fin du mur Xdebut, Ydebut Xfin, Yfin  en float
 //ainsi qu'un entier qui nous renseigne sur la présence du mur (0 = pas de mur; 1 = mur présent)
-struct s_mur{
+typedef struct {
     float Xdebut;
     float Ydebut;
     float Xfin;
     float Yfin;
     int existe;
-}
+}s_mur;
 
-float scanlight[8] = [0 0 0 0 0 0 0 0]; // tableau correspondant au scan light pour les 8 zones
+typedef struct {
+float zone_distance[8]; // tableau correspondant au scan light pour les 8 zones
+}scanlight;
 
-struct scanfin{
-    float dist_scan[10] = [0 0 0 0 0 0 0 0 0 0]; // les 10 distances les plus courtes pour le scan selectionnée
-    int type_scan[10] = {}; //type d'objet scanné sur la zone scannée
-}
+typedef struct {
+    float dist_scan[10]; // les 10 distances les plus courtes pour le scan selectionnée
+    int type_scan[10]; //type d'objet scanné sur la zone scannée
+}scanfin;
 
 
 /*Initialisation des connexions TCP/IP
@@ -46,12 +48,12 @@ prend en paramètre :
 	- l'adresse ip du serveur pour la connexion
 	- le port pour la connexion
 renvoie id du joueur, renvoie -1 si problème de connexion, code d'erreur à préciser par la suite*/
-int init_connexion(char[40] nom, char[15] ip, int port);
+int init_connexion(char nom[40], char ip[15], int port);
 
 /*permet de récupérer la position d'un robot:
 pas de parametre
-renvoie une structure robot*/
-robot get_position();
+renvoie une structure s_robot*/
+s_robot* get_position(int id);
 
 /*permet de demander de déplacer un robot
 prend en parametre :
@@ -63,10 +65,10 @@ void move(float speed, float teta);
 /*permet de connaitre l'etat du robot, pour savoir si il touche un objet (mur, autre robot)
 pas de parametre
 renvoie 1 char (8bits) pour connaitre quel zone est en contact (bit0 pour zone 1 ... bit7 pour zone 8*/
-char get_status();
+char get_status(int id);
 
 //TODO
-close_connexion();
+void close_connexion();
 
 /*permet de connaitre le niveau de vie d'un robot
 - pas de parametre
@@ -76,7 +78,7 @@ int getlife();
 /*permet au robot de scanner grossierement à 360° les obstacles sur 8 zones et renvoie la distance la plus courte pour chaque zone
 - pas de parametre
 - renvoie un pointeur vers tableau de 8 floats (1 par zone) correspondant à la distance la plus courte*/
-scanlight scan_light();
+scanlight* scan_light();
 
 /*permet au robot de scanner finement les obstacles sur une zone de 30° (12 zones) et renvoie les 10 objets les plus proches
 prend en parametre:
@@ -87,13 +89,13 @@ renvoie un pointeur vers la structure scanfin (10 objets les plus proches et leu
 2 = robot ennemi
 3 = missile
 */
-scanfin scan_fin(int zone);
+scanfin* scan_fin(int zone);
 
 /*permet au robot de tirer un missile dans une direction donnée
 prend en parametre:
     - l'angle de tir en radian (float)
     - ne renvoie rien*/
-void missile_shoot(float teta):
+void missile_shoot(float teta);
 
 /*permet de connaitre le nombre de missiles tirés non explosés
 pas de parametre
