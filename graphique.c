8f48d274e68ae48bc935cd829eaf9c4f7790091c
nezyl <smacksi@hotmail.fr>
2015-12-04 17:39:59 +0100

    Update graphique.c

--- graphique.c
+++ graphique.c
@@ -1,8 +1,5 @@
-//graphique
-//http://www.libsdl.org/index.php
-//http://www.gtk.org/
-//gcc -o main main.c `sdl-config --cflags --libs` 
-//./main
+//gcc -o graphique graphique.c `sdl-config --cflags --libs` 
+//./graphique
 
 
 
@@ -11,137 +8,198 @@
 #include <SDL/SDL.h>
 #include "SDL/SDL_image.h"
 
-int main(int argc, char *argv[])
+
+
+
+
+int graphique(int largeur_ecran, int hauteur_ecran, int larg_score, int posix,int posiy)
 {
-    /*
-    1. Déclaration des variables
-    */
-    SDL_Surface *ecran = NULL, *zozor = NULL;
-    SDL_Surface *clotureVerticale = NULL, *clotureHorizontale = NULL;
-    SDL_Event event;
-    SDL_Rect positionZozor;
-    SDL_Rect positionClotureGH, positionClotureD, positionClotureB;
-    int continuer = 1;
 
 
-    /*
-    2. On s'occupe de la fenêtre
-    */
-    SDL_Init(SDL_INIT_VIDEO);
+	/*
+	1 - Déclaration des variables
+	*/
+	SDL_Surface *ecran = NULL; 
+	SDL_Surface *robot = NULL; 
+	SDL_Surface *terrain = NULL;
+	SDL_Surface *bandeau_score = NULL;
+	SDL_Surface *mur = NULL;
 
-    ecran = SDL_SetVideoMode(640, 480, 32, SDL_HWSURFACE | SDL_DOUBLEBUF);
-    SDL_WM_SetCaption("Pauvre Zozor.", NULL);
-    //On colore le fond en vert (imitation herbe... :lol: )
-    SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 11,133,14));
-    //SDL_FillRect(ecran, NULL, "Herbe.bmp");
+	SDL_Event event;
 
+	SDL_Rect positionTerrain;
+	SDL_Rect positionRobot;
+	SDL_Rect positionScore;
+	SDL_Rect positionMur;
 
-
-    /*
-    3. Les clôtures
-    */
-    // On détermine les dimensions des clôtures verticales et horizontales
-    clotureVerticale = SDL_CreateRGBSurface(SDL_HWSURFACE, 10, ecran->h - 2*40, 32, 0, 0, 0, 0);
-    clotureHorizontale = SDL_CreateRGBSurface(SDL_HWSURFACE, ecran->w - 2*40, 10, 32, 0, 0, 0, 0);
-
-    // On détermine la position des 4 clôtures
-    // Celle de gauche et celle du haut (elles ont la même origine)
-    positionClotureGH.x = 40;
-    positionClotureGH.y = 40;
-    // Celle de droite
-    positionClotureD.x = ecran->w - 50;
-    positionClotureD.y = 40;
-    // Celle du bas
-    positionClotureB.x = 40;
-    positionClotureB.y = ecran->h -50;
-
-    // On blitte les 4 clôtures aux bons endroits
-    //Celle de gauche
-    SDL_BlitSurface(clotureVerticale, NULL, ecran, &positionClotureGH);
-    //Celle de droite
-    SDL_BlitSurface(clotureVerticale, NULL, ecran, &positionClotureD);
-    //Celle du haut
-    SDL_BlitSurface(clotureHorizontale, NULL, ecran, &positionClotureGH);
-    //Celle du bas
-    SDL_BlitSurface(clotureHorizontale, NULL, ecran, &positionClotureB);
+	int longu_mur =100;
+	int haut_mur = 5;
+	int marge =5;
+	int continuer = 1;
+	
+	
 
 
-    /*
-    4. Placement initial de Zozor
-    */
-    //On charge l'image de Zozor dans la surface et on rend le fond transparent
-    zozor = SDL_LoadBMP("tank.bmp");
-    SDL_SetColorKey(zozor, SDL_SRCCOLORKEY, SDL_MapRGB(zozor->format, 255, 255, 255));
-
-    // On rend le curseur invisible et on le centre (Zozor devient le curseur)
-    SDL_ShowCursor(SDL_DISABLE);
-    SDL_WarpMouse((ecran->w / 2 - zozor->w / 2), (ecran->h / 2 - zozor->h / 2));
+	/*
+	2 - SDL_Init : chargement de la SDL
+	*/
+	if (SDL_Init(SDL_INIT_VIDEO) == -1) // Démarrage de la SDL. Si erreur :
+		{
+		fprintf(stderr, "Erreur d'initialisation de la SDL : %s\n", SDL_GetError()); // Écriture de l'erreur
+		exit(EXIT_FAILURE); // On quitte le programme
+		}
 
 
-    /*
-    5. La boucle principale
-    */
-    while(continuer)
-    {
-
-        SDL_WaitEvent(&event);
-
-        switch(event.type)
-        {
-            case SDL_QUIT:
-                continuer = 0;
-                break;
-
-            // En appuyant sur une touche, on quitte le programme
-            case SDL_KEYDOWN:
-                continuer = 0;
-                break;
-
-            // On limite les déplacements de Zozor à son enclos (mouahaha)
-            case SDL_MOUSEMOTION:
-            // Empêcher le dépassement vers la droite
-                if(event.motion.x > ecran->w - zozor->w - 50)
-                    SDL_WarpMouse(ecran->w - zozor->w - 50, event.motion.y);
-
-            // Empêcher le dépassement vers la gauche
-                if(event.motion.x < 50)
-                    SDL_WarpMouse(50, event.motion.y);
-
-            // Empêcher le dépassement vers le haut
-                if(event.motion.y < 50)
-                    SDL_WarpMouse(event.motion.x, 50);
-
-            // Empêcher le dépassement vers le bas
-                if(event.motion.y > ecran->h - zozor->h - 50)
-                    SDL_WarpMouse(event.motion.x, ecran->h - zozor->h - 50);
-
-                positionZozor.x = event.motion.x;
-                positionZozor.y = event.motion.y;
-                break;
-        }
-
-        // On efface et on blitte à nouveau toutes les surfaces aux éventuels nouveaux emplacements
-        SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 0, 255, 0));
-        // Zozor
-        SDL_BlitSurface(zozor, NULL, ecran, &positionZozor);
-        // Les clotures
-        SDL_BlitSurface(clotureVerticale, NULL, ecran, &positionClotureGH);
-        SDL_BlitSurface(clotureVerticale, NULL, ecran, &positionClotureD);
-        SDL_BlitSurface(clotureHorizontale, NULL, ecran, &positionClotureGH);
-        SDL_BlitSurface(clotureHorizontale, NULL, ecran, &positionClotureB);
-
-        SDL_Flip(ecran);
-    }
+	/*
+	3 - Utilisation d'une icone de fenetre
+	*/
+	SDL_WM_SetIcon(SDL_LoadBMP("tank.bmp"), NULL);
 
 
-    /*
-    6. Pour terminer
-    */
-    SDL_Flip(ecran);
-    SDL_FreeSurface(zozor);
-    SDL_FreeSurface(clotureVerticale);
-    SDL_FreeSurface(clotureHorizontale);
-    SDL_Quit();
+	/*
+	4 - Création d'une fenètre
+	*/
+	ecran = SDL_SetVideoMode(largeur_ecran, hauteur_ecran, 32, SDL_HWSURFACE | SDL_DOUBLEBUF | SDL_RESIZABLE); // définition de la fenetre
+	if (ecran == NULL) // Si l'ouverture a échoué, on le note et on arrête
+	{
+		fprintf(stderr, "Impossible de charger le mode vidéo : %s\n", SDL_GetError());
+		exit(EXIT_FAILURE);
+	}
+	SDL_WM_SetCaption("Sirop bot", NULL); //titre de la fenetre
+	SDL_FillRect(ecran, NULL, SDL_MapRGB(ecran->format, 128, 128, 128)); //on colore l'ecran en gris
 
-    return EXIT_SUCCESS;
+
+	/*
+	5 - On s'occupe du terrain de jeu
+	*/
+	terrain=SDL_LoadBMP("sol.bmp");
+	// On détermine la position du terrain
+	positionTerrain.x = marge;
+	positionTerrain.y = marge;
+
+
+	/*
+	6 - On s'occupe du robot
+	*/
+	robot=SDL_LoadBMP("Robot_jaune.bmp");
+	//on rend le fond blanc de l'image transparent
+	SDL_SetColorKey(robot, SDL_SRCCOLORKEY, SDL_MapRGB(robot->format, 255, 255, 255));
+	// On détermine la position du terrain
+	positionRobot.x = posix;
+	positionRobot.y = posiy;
+
+
+
+	/*
+	7 - On s'occupe du bandeau de score
+	*/
+	bandeau_score = SDL_CreateRGBSurface(SDL_HWSURFACE, larg_score, hauteur_ecran, 32, 255, 0, 0, 0);
+	SDL_FillRect(bandeau_score, NULL, SDL_MapRGB(bandeau_score->format, 102, 178, 255)); //on colore l'ecran en bleu
+	// On détermine la position du terrain
+	positionScore.x = largeur_ecran - larg_score -(2*marge);
+	positionScore.y = marge;
+	// On blitte le terrain au bon endroit
+	SDL_BlitSurface(bandeau_score, NULL, ecran, &positionScore);
+
+
+
+
+	/*
+	5. On s'occupe du mur
+	*/
+	mur = SDL_CreateRGBSurface(0, longu_mur, haut_mur, 32, 0, 0, 0, 0);
+	// On détermine la position du terrain
+	positionMur.x = 10;
+	positionMur.y = 10;
+	// On blitte le terrain au bon endroit
+	SDL_BlitSurface(mur, NULL, terrain, &positionMur);
+
+	/*
+	5 - On rafraichie
+	*/
+	SDL_Flip(ecran);
+
+
+
+
+	// Activation de la répétition des touches
+	SDL_EnableKeyRepeat(100, 100);
+
+
+
+	/*
+	5. La boucle principale
+	*/
+	while(continuer)
+	{
+	
+	SDL_WaitEvent(&event);
+		switch(event.type)
+		{
+			
+			// En appuyant sur une echap, on quitte le programme
+			case SDL_QUIT:
+			continuer = 0;
+			break;
+
+			// En appuyant sur une touche, on quitte le programme
+			case SDL_KEYDOWN:
+				switch(event.key.keysym.sym)
+				{
+				    case SDLK_ESCAPE:
+					continuer = 0;
+					break;
+				    case SDLK_UP:
+					positionRobot.y = positionRobot.y -5; 
+					
+					break;
+				    case SDLK_DOWN:
+					positionRobot.y = positionRobot.y +5; 
+					
+					break;
+				    case SDLK_RIGHT:
+					positionRobot.x = positionRobot.x +5; 
+					
+					break;
+				    case SDLK_LEFT:
+					positionRobot.x = positionRobot.x -5;
+					
+					break;
+							
+				}
+			break;
+		}
+		// On affiche avec les nouvelles valeurs
+		SDL_BlitSurface(terrain, NULL, ecran, &positionTerrain);
+		SDL_BlitSurface(robot, NULL, ecran, &positionRobot);
+		SDL_Flip(ecran);
+	}
+	
+
+
+	/*
+	6. Pour terminer
+	*/
+	SDL_Flip(ecran);
+	SDL_FreeSurface(terrain);
+	SDL_FreeSurface(bandeau_score);
+	SDL_FreeSurface(robot);
+
+
+	/*
+	SDL_Quit : arrêt de la SDL
+	*/
+	SDL_Quit();
+
+	return EXIT_SUCCESS;
 }
+
+
+int main (int argc, char *argv[])
+{
+
+	graphique (1100,800,200,40,40);
+
+
+}
+
